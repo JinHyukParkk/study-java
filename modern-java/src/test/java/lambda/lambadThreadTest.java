60ebb5c22555b8fd939dc5c068733ef38ee86d61
hyuk <pjh08190819@naver.com>
2022-01-24 23:59:46 +0900

    :see_no_evil: test thread lambda

---
+++ modern-java/src/test/java/lambda/lambadThreadTest.java
@@ -0,0 +1,37 @@
+package lambda;
+
+import org.junit.jupiter.api.Test;
+
+public class lambadThreadTest {
+
+    @Test
+    void runnableTest() {
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("Start Thread");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("End Thread");
+            }
+        });
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println("Start Thread");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("End Thread");
+        });
+
+        thread.start();
+
+        thread1.run();
+    }
+}
