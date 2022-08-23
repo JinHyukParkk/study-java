827a57ae5097bcbb0b8a3c6d9abebfcd50400bdc
hyuk <pjh08190819@naver.com>
2022-08-24 01:58:14 +0900

    test : Stream reduce 테스트

---
+++ modern-java/src/test/java/stream/StreamReduce.java
@@ -0,0 +1,42 @@
+package stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamReduce {
+
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+
+    @Test
+    void sumTest() {
+        int sum = numbers.stream().reduce(0, Integer::sum);
+
+        System.out.println(sum);
+    }
+
+    @Test
+    void sumTestWithoutInitValue() {
+        Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+
+        System.out.println(sum);
+    }
+
+    @Test
+    void sumTestWithoutInitValueAndList() {
+        List<Integer> numbers = Arrays.asList();
+
+        Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+
+        System.out.println(sum);
+    }
+
+    @Test
+    void maxTest() {
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+
+        System.out.println(max);
+    }
+}
