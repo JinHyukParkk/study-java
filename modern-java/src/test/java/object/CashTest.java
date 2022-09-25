944406d293966ec7a90ba6d2ff46340ed4694a88
hyuk <pjh08190819@naver.com>
2022-09-25 18:55:26 +0900

    study : Fake 객체 추가

--- modern-java/src/test/java/object/CashTest.java
+++ modern-java/src/test/java/object/CashTest.java
@@ -8,12 +8,21 @@ import org.mockito.Mockito;
 class CashTest {
 
     @Test
-    void test() {
+    void testMock() {
         Exchange exchange = Mockito.mock(Exchange.class);
         Mockito.doReturn(1234)
                 .when(exchange)
                 .rate("USD", "EUR");
 
+        Cash dollar = new Cash(exchange, 500);
+        Cash euro = dollar.in("EUR");
+
+        assertThat(500*1234).isEqualTo(euro.getCents());
+    }
+
+    @Test
+    void testFake() {
+        Exchange exchange = new FakeExchange();
         Cash dollar = new Cash(exchange, 500);
         Cash euro = dollar.in("EUR");
 
