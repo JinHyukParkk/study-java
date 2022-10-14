415b2c9ff155c3194275a512ba2648cbb3b9cc22
hyuk <pjh08190819@naver.com>
2022-10-14 10:14:58 +0900

    study : optional 스트림 처리 테스트

--- modern-java/src/main/java/optional/Car.java
+++ modern-java/src/main/java/optional/Car.java
@@ -1,5 +1,16 @@
 package optional;
 
+import java.util.Optional;
+
 public class Car {
 
+    private Optional<Insurance> insurance;
+
+    public Car(Optional<Insurance> insurance) {
+        this.insurance = insurance;
+    }
+
+    public Optional<Insurance> getInsurance() {
+        return insurance;
+    }
 }
--- modern-java/src/main/java/optional/Person.java
+++ modern-java/src/main/java/optional/Person.java
@@ -4,9 +4,13 @@ import java.util.Optional;
 
 public class Person {
 
-    private Car car;
+    private Optional<Car> car;
 
-    private Optional<Car> getCarAsOptional() {
-        return Optional.ofNullable(car);
+    public Person(Optional<Car> car) {
+        this.car = car;
+    }
+
+    public Optional<Car> getCar() {
+        return car;
     }
 }
