fd5c6eb030e428a47b577d04915b9fba347b6646
hyuk <pjh08190819@naver.com>
2022-10-18 23:59:04 +0900

    study : LocalDate 절대적인 방식 테스트

--- modern-java/src/test/java/date/LocalDateExampleTest.java
+++ modern-java/src/test/java/date/LocalDateExampleTest.java
@@ -2,6 +2,7 @@ package date;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.temporal.ChronoField;
 import org.junit.jupiter.api.Test;
 
 class LocalDateExampleTest {
@@ -13,5 +14,35 @@ class LocalDateExampleTest {
         System.out.println(year);
         Month month = date.getMonth();
         System.out.println(month);
+    }
+
+    @Test
+    void testChronoField() {
+        LocalDate nowDate = LocalDate.now();
+
+        int year = nowDate.get(ChronoField.YEAR);
+        int month = nowDate.get(ChronoField.MONTH_OF_YEAR);
+        int day = nowDate.get(ChronoField.DAY_OF_MONTH);
+
+        System.out.println(year);
+        System.out.println(month);
+        System.out.println(day);
+    }
+
+    @Test
+    void testWithAttribute() {
+        LocalDate date = LocalDate.of(2022, 10, 18);
+        System.out.println(date);
+
+        LocalDate date1 = date.withYear(2021);
+        System.out.println(date1);
+
+        // withDayOfMonth 그 달의 N일
+        LocalDate date2 = date1.withDayOfMonth(25);
+        System.out.println(date2);
+
+        // withDayOfYear 그 해의 N일
+        LocalDate date3 = date2.withDayOfYear(233);
+        System.out.println(date3);
     }
 }
