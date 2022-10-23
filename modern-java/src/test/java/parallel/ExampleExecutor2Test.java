5124a8d9542078ff065a612cc62a74094ae13ca3
hyuk <pjh08190819@naver.com>
2022-10-24 00:55:13 +0900

    study : future 테스트

---
+++ modern-java/src/test/java/parallel/ExampleExecutor2Test.java
@@ -0,0 +1,11 @@
+package parallel;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleExecutor2Test {
+
+    @Test
+    void test() throws InterruptedException {
+        new ExampleExecutor2().execute();
+    }
+}
