dd105d2c29d55901e42a155a9265df2032300c60
hyuk <pjh08190819@naver.com>
2022-10-28 23:40:02 +0900

    study : Iterable 인터페이스 디폴트 메서드 테스트

---
+++ modern-java/src/main/java/collection/ArrayListExample.java
@@ -0,0 +1,11 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+    public void create() {
+        ArrayList<String> strings = new ArrayList<>();
+
+    }
+}
---
+++ modern-java/src/test/java/collection/ArrayListExampleTest.java
@@ -0,0 +1,33 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Spliterator;
+import org.junit.jupiter.api.Test;
+
+class ArrayListExampleTest {
+
+    @Test
+    void testDefaultMethodInIterable() {
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            strings.add(String.format("test %d", i));
+        }
+
+        // Iterable 디폴트 메서드 (foreach)
+        long startTime = System.nanoTime();
+
+        strings.forEach((i) -> {
+        });
+
+        System.out.println((System.nanoTime() - startTime) / 1000000 + "밀리초 걸림");
+
+        // Iterable 디폴트 메서드(spliterator)
+        startTime = System.nanoTime();
+
+        Spliterator<String> spliterator = strings.spliterator();
+        spliterator.forEachRemaining((i) -> {
+        });
+
+        System.out.println((System.nanoTime() - startTime) / 1000000 + "밀리초 걸림");
+    }
+}
