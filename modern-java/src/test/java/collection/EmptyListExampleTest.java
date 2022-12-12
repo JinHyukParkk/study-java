1f9b38eaf213781816d98bb161a808d8b3525f65
hyuk <pjh08190819@afreecatv.com>
2022-12-12 17:44:23 +0900

    style : 안쓴 테스트 제거

--- modern-java/src/test/java/collection/EmptyListExampleTest.java
+++ modern-java/src/test/java/collection/EmptyListExampleTest.java
@@ -1,5 +1,6 @@
 package collection;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,20 +21,6 @@ class EmptyListExampleTest {
 
     @Test
     void emptyListTest_case2() {
-        List<Integer> empty = Collections.EMPTY_LIST;
-
-        try {
-            empty.add(1);
-        } catch (Exception e) {
-            System.out.printf("class : %s, message : %s%n", e.getClass(), e.getMessage());
-        }
-
-        // uses unchecked or unsafe operations.
-        //Note: Recompile with -Xlint:unchecked for details. g형변환 ?
-    }
-
-    @Test
-    void emptyListTest_case3() {
         List<Integer> empty = Collections.EMPTY_LIST;
 
         try {
