ce1264a01103542194ea42872e52b00a1d5322a9
hyuk <pjh08190819@afreecatv.com>
2022-12-12 17:42:07 +0900

    study : emptyList() 테스트

---
+++ modern-java/src/test/java/collection/EmptyListExampleTest.java
@@ -0,0 +1,48 @@
+package collection;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class EmptyListExampleTest {
+
+    @Test
+    void emptyListTest_case1() {
+        List<Integer> empty = Collections.emptyList();
+
+        try {
+            empty.add(1);
+        } catch (Exception e) {
+            System.out.printf("class : %s, message : %s%n", e.getClass(), e.getMessage());
+        }
+    }
+
+    @Test
+    void emptyListTest_case2() {
+        List<Integer> empty = Collections.EMPTY_LIST;
+
+        try {
+            empty.add(1);
+        } catch (Exception e) {
+            System.out.printf("class : %s, message : %s%n", e.getClass(), e.getMessage());
+        }
+
+        // uses unchecked or unsafe operations.
+        //Note: Recompile with -Xlint:unchecked for details. g형변환 ?
+    }
+
+    @Test
+    void emptyListTest_case3() {
+        List<Integer> empty = Collections.EMPTY_LIST;
+
+        try {
+            empty.add(1);
+        } catch (Exception e) {
+            System.out.printf("class : %s, message : %s%n", e.getClass(), e.getMessage());
+        }
+
+        // uses unchecked or unsafe operations.
+        //Note: Recompile with -Xlint:unchecked for details. g형변환 ?
+    }
+}
