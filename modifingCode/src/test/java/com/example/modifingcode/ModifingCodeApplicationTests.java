3334b8520d0f776ae973c255d5f0521b491a6b51
hyuk <pjh08190819@naver.com>
2022-12-23 21:17:59 +0900

    init : 코드 조작 프로젝트 초기 세팅

---
+++ modifingCode/src/test/java/com/example/modifingcode/ModifingCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modifingcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModifingCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
