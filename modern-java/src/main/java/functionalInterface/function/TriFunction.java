5c831ed5103370fa0c51784414c41e5bb21e3cfd
hyuk <pjh08190819@naver.com>
2023-01-18 00:55:11 +0900

    study : 세 개의 인자 받아서 더하는 함수형 인터페이스 만들기

---
+++ modern-java/src/main/java/functionalInterface/function/TriFunction.java
@@ -0,0 +1,7 @@
+package functionalInterface.function;
+
+@FunctionalInterface
+public interface TriFunction<T, U, V, R> {
+
+    R apply(T t, U u, V v);
+}
---
+++ modern-java/src/test/java/functionalInterface/function/TriFunctionTest.java
@@ -0,0 +1,15 @@
+package functionalInterface.function;
+
+import org.junit.jupiter.api.Test;
+
+class TriFunctionTest {
+
+    @Test
+    void test() {
+        TriFunction<Integer, Integer, Integer, Integer> addThreeNumbers =
+                (x, y, z) -> x + y + z;
+
+        int result = addThreeNumbers.apply(1, 2, 3);
+        System.out.println(result);
+    }
+}
