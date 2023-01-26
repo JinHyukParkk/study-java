c25ef5e2b4dd7bbf82d1c68ef721e6ae4cf7a66d
hyuk <pjh08190819@naver.com>
2023-01-27 03:40:25 +0900

    study : Predicate 예제 테스트

--- javastream/src/test/java/org/example/PredicateExampleTest.java
+++ javastream/src/test/java/org/example/PredicateExampleTest.java
@@ -16,8 +16,16 @@ class PredicateExampleTest {
 
     @Test
     void test1() {
+        Predicate<Integer> isPositive = x -> x > 0;
         List<Integer> inputs = Arrays.asList(10, -5, 4, -2, 0);
-        System.out.println("Positive number : " + filter(inputs, x -> x > 0));
+        System.out.println("Positive number : "
+                + filter(inputs, x -> x > 0));
+        System.out.println("Non-positive number : "
+                + filter(inputs, isPositive.negate()));
+        System.out.println("Non-negative number : "
+                + filter(inputs, isPositive.or(x -> x == 0)));
+        System.out.println("Positive even numbers : "
+                + filter(inputs, isPositive.and(x -> x % 2 == 0)));
     }
 
     private <T> List<T> filter(List<T> inputs, Predicate<T> condition) {
