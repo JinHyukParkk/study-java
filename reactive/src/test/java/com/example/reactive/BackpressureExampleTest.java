8b1ea7901a49bc862cb3178fa252085c02009b48
hyuk <hyuk@woowahan.com>
2023-03-18 01:43:31 +0900

    study : Flux 처리 테스트

--- reactive/src/test/java/com/example/reactive/BackpressureExampleTest.java
+++ reactive/src/test/java/com/example/reactive/BackpressureExampleTest.java
@@ -21,16 +21,39 @@ class BackpressureExampleTest {
             .blockLast();
     }
 
+    /*
+    * StepVerifier 는 verify 관련 메서드로 끝내야 함
+    * */
     @Test
-    @DisplayName("첫번째 요소를 가져와 확인하는 방법")
+    @DisplayName("첫번째 요소를 가져와 순차적으로 확인하는 방법")
     void test2() {
-        Flux<Integer> flux = Flux.just(0, 1, 2);
+        Flux<Integer> flux = Flux.just(0, 1, 2, 3).log();
 
         StepVerifier.create(flux ,1)
             .expectNext(0)
             .thenRequest(1)    // 다음 Request 요청
             .expectNext(1)
             .thenRequest(1)
-            .expectNext(2);
+            .expectNext(2)
+            .thenCancel()       // 다음 요소를 구독하지 않고, StepVerifier 반환
+            .verify();
+    }
+
+    @Test
+    @DisplayName("요소들 로그 찍기")
+    void test3() {
+        Flux.just(0, 1, 2).log()
+            .doOnNext(System.out::println)
+            .blockLast();
+    }
+
+    @Test
+    @DisplayName("Flux 여러 처리 과정 테스트")
+    void test4() {
+        Flux.just(0, 1, 2)
+            .doOnSubscribe(s -> System.out.println("Starring :"))
+            .doOnNext(System.out::println)
+            .doOnComplete(() -> System.out.println("The end!"))
+            .blockLast();
     }
 }
