38cb385e233181f82ceaf57aa316fd98c67513cb
hyuk <hyuk@woowahan.com>
2023-03-17 23:57:57 +0900

    study : Flux 첫번째 요소 확인하는 테스트

--- reactive/src/test/java/com/example/reactive/BackpressureExampleTest.java
+++ reactive/src/test/java/com/example/reactive/BackpressureExampleTest.java
@@ -3,6 +3,7 @@ package com.example.reactive;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
 
@@ -18,5 +19,18 @@ class BackpressureExampleTest {
         Flux<Long> flux = Flux.interval(Duration.ofMillis(100)).take(4).log();
         flux.doOnNext(System.out::println)
             .blockLast();
+    }
+
+    @Test
+    @DisplayName("첫번째 요소를 가져와 확인하는 방법")
+    void test2() {
+        Flux<Integer> flux = Flux.just(0, 1, 2);
+
+        StepVerifier.create(flux ,1)
+            .expectNext(0)
+            .thenRequest(1)    // 다음 Request 요청
+            .expectNext(1)
+            .thenRequest(1)
+            .expectNext(2);
     }
 }
