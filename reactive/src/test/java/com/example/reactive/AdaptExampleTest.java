07a0d5496183aa0b4d1310d6d29838d3ec14a5cd
hyuk <hyuk@woowahan.com>
2023-03-24 02:59:08 +0900

    study : RxJava에서 adapt 역할해주는 메서드 테스트

---
+++ reactive/src/test/java/com/example/reactive/AdaptExampleTest.java
@@ -0,0 +1,68 @@
+package com.example.reactive;
+
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+
+class AdaptExampleTest {
+
+    @Test
+    @DisplayName("어떠한 publisher를 바꿔 낄 수 있는 flowable - Flux에서 Flowable로 바꾸기")
+    void case1() {
+        Flowable<Integer> flowable = Flowable.fromPublisher(Flux.just(2));
+
+        Flux.from(flowable);
+
+    }
+
+    /*
+     *  Observable 은 backpressure가 없는거 같음. 그래서 Flux를 만들 때 Backpressure 설정을 하는 듯.
+     * */
+    @Test
+    @DisplayName("Flux를 Observable로 바꾸기")
+    void case2() {
+        Flux<Integer> flux = Flux.just(2, 3);
+        Observable<Integer> observable = Observable.just(2);
+
+        Flux.from(observable.toFlowable(BackpressureStrategy.BUFFER));
+        Observable.fromPublisher(flux);
+
+        flux.log()
+            .doOnNext(System.out::println)
+            .blockLast();
+    }
+
+    @Test
+    @DisplayName("Mono를 Single로, Single을 Mono로")
+    void case3() {
+        Mono<Integer> mono = Mono.just(2);
+        Single<Integer> single = Single.just(2);
+
+        Mono.from(single.toFlowable());
+        Single.fromPublisher(mono);
+    }
+
+    @Test
+    @DisplayName("Future를 Mono로 바꿔보기")
+    void case4() {
+        CompletableFuture future = CompletableFuture.supplyAsync(() -> "hello");
+        future.thenApply(s -> s.toString().toUpperCase());
+
+        Mono<String> mono = Mono.just("hello")
+            .map(String::toUpperCase);
+
+        Mono.fromFuture(future);
+    }
+
+    /*
+     * reator 안에는 이렇게 Adapter 코드들이 내제되어 있을거임
+     *
+     * */
+}
--- reactive/src/test/java/com/example/reactive/ErrorExceptionTest.java
+++ reactive/src/test/java/com/example/reactive/ErrorExceptionTest.java
@@ -2,6 +2,7 @@ package com.example.reactive;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,5 +27,25 @@ class ErrorExceptionTest {
         mono.log().onErrorResume(e -> Mono.just(2))
             .doOnNext(System.out::println)
             .subscribe();
+    }
+
+    @Test
+    @DisplayName("Mono 처리 중 map 과정에서 에러 발생 시 200 값으로 처리한다.")
+    void case3() {
+        Mono.just("hello")
+            .log()
+            .map(s -> {
+                int value = 0;
+                try {
+                    Integer.parseInt(s);
+                } catch (Exception e) {
+                    throw Exceptions.propagate(e);  // Checked Exception 을 Unchecked Exception 으로 감싸줌
+                }
+                return value;
+            })
+            .onErrorReturn(200)
+            .doOnNext(System.out::println)
+            .subscribe();
+
     }
 }
