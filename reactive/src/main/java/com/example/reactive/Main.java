157b95a19e390e5df1e5b4a5c4ad9ffc7d13e25e
hyuk <hyuk@woowahan.com>
2023-04-01 15:22:36 +0900

    study : 비동기 환경에서 blocking code 성능 개선을 위한 방법 주석 작성

--- reactive/src/main/java/com/example/reactive/Main.java
+++ reactive/src/main/java/com/example/reactive/Main.java
@@ -2,10 +2,17 @@ package com.example.reactive;
 
 public class Main {
 
+    /*
+     * blocking code 로 대표적인게 JDBC Connection 과 그 쿼리들이 있다.
+     * 이 blocking code를 비동기로 바꾸고 성능 향상을 위해 'Scheduler'를 이용하자.
+     *
+     * */
+
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test");
 
         String profile = "Hi, I am Hyuk";
+        String reason = "I study react";
     }
 }
