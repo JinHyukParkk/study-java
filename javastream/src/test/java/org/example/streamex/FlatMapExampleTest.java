cf3baeb94d827e36e128f5cd24be151984263a7f
hyuk <hyuk@woowahan.com>
2023-04-23 16:40:25 +0900

    test : flatMap 연산 풀어 쓰기

--- javastream/src/test/java/org/example/streamex/FlatMapExampleTest.java
+++ javastream/src/test/java/org/example/streamex/FlatMapExampleTest.java
@@ -3,6 +3,7 @@ package org.example.streamex;
 import org.example.fixture.OrderFixtureFactory;
 import org.example.models.Order;
 import org.example.models.OrderLine;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -27,11 +28,25 @@ class FlatMapExampleTest {
     }
 
     @Test
+    @DisplayName("flatMap을 이용한 Order -> OrderLine 변환")
     void case2() {
         List<Order> orders = OrderFixtureFactory.createOrderWithOrderLine();
 
         List<OrderLine> orderLineList = orders.stream()
             .flatMap(order -> order.getOrderLines().stream())
+            .collect(Collectors.toList());
+
+        System.out.println(orderLineList);
+    }
+
+    @Test
+    @DisplayName("flatMap을 이용한 Order -> OrderLine 변환 2")
+    void case3() {
+        List<Order> orders = OrderFixtureFactory.createOrderWithOrderLine();
+
+        List<OrderLine> orderLineList = orders.stream()
+            .map(Order::getOrderLines)
+            .flatMap(List::stream)
             .collect(Collectors.toList());
 
         System.out.println(orderLineList);
