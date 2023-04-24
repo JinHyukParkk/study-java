acae66c70d69464f82bb4b5cc1c88449f4f209bd
hyuk <hyuk@woowahan.com>
2023-04-24 23:57:43 +0900

    chore : NPE 설명 추가

---
+++ javastream/src/main/java/org/example/optionalex/OptionalExample.java
@@ -0,0 +1,14 @@
+package org.example.optionalex;
+
+public class OptionalExample {
+
+    /*
+     *
+     * NPE - NullPointException
+     * Null 상태인 오브젝트를 레퍼런스 할 때 발생
+     * Runtime error이기 때문에 실행 전까지는 발생 여부를 알기 쉽지 않음
+     * "[Null pointer를 발명한 것은] 나의 10억 불 짜리 실수 였다 - 토니 호어 (한 강연에서)
+     *
+     * */
+
+}
