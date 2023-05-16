64024bfda8fd5246566d08f9b33e78850ebcdf26
hyuk <hyuk@woowahan.com>
2023-05-17 02:00:19 +0900

    study : reactive error 처리 전략 테스트

--- reactive/src/test/java/com/example/reactive/ErrorExceptionTest.java
+++ reactive/src/test/java/com/example/reactive/ErrorExceptionTest.java
@@ -3,9 +3,8 @@ package com.example.reactive;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.Exceptions;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ErrorExceptionTest {
 
@@ -47,5 +46,91 @@ class ErrorExceptionTest {
             .doOnNext(System.out::println)
             .subscribe();
 
+    }
+
+    @Test
+    @DisplayName("subscribe에서 에러 발생 시 핸들링 해본다.")
+    void case4() {
+        Flux.just(1, 2, 3)
+            .map(i -> {
+                if (i == 2) {
+                    throw new RuntimeException("error");
+                }
+                return i;
+            })
+            .subscribe(
+                (data) -> System.out.println(data),
+                (error) -> System.out.println(error),
+                () -> System.out.println("complete"));
+    }
+
+    @Test
+    @DisplayName("onErrorReturn으로 에러 발생시 -1 을 넘긴다.")
+    void case5() {
+        Flux.just(1, 2, 3)
+            .map(i -> {
+                if (i == 2) {
+                    throw new RuntimeException("error");
+                }
+                return i;
+            })
+            .onErrorReturn(-1)
+            .subscribe(
+                (data) -> System.out.println(data),
+                (error) -> System.out.println(error),
+                () -> System.out.println("complete"));
+    }
+
+    @Test
+    @DisplayName("onErrorResume으로 에러 발생 시 새로운 스트림을 생성한다.")
+    void case6() {
+        Flux.just(1, 2, 3)
+            .map(i -> {
+                if (i == 2) {
+                    throw new RuntimeException("error");
+                } else if (i == -2) {
+                    throw new RuntimeException("error2");
+                }
+                return i;
+            })
+            .onErrorResume(e -> Flux.just(-1, -2, -3))
+            .subscribe(
+                (data) -> System.out.println(data),
+                (error) -> System.out.println(error),
+                () -> System.out.println("complete"));
+    }
+
+    @Test
+    @DisplayName("onErrorMap으로 에러 발생 시 새로운 에러를 생성한다.")
+    void case7() {
+        Flux.just(1, 2, 3)
+            .map(i -> {
+                if (i == 2) {
+                    throw new RuntimeException("error");
+                }
+                return i;
+            })
+            .onErrorMap(error -> new IllegalArgumentException("error"))
+            .subscribe(
+                (data) -> System.out.println(data),
+                (error) -> System.out.println(error),
+                () -> System.out.println("complete"));
+    }
+
+    @Test
+    @DisplayName("onErrorContinue로 에러 발생 시 에러를 처리하고, 나머지 스트림을 처리한다.")
+    void case8() {
+        Flux.just(1, 2, 3)
+            .map(i -> {
+                if (i == 2) {
+                    throw new RuntimeException("error");
+                }
+                return i;
+            })
+            .onErrorContinue((error, data) -> System.out.println("error: " + error + ", data: " + data))
+            .subscribe(
+                (data) -> System.out.println(data),
+                (error) -> System.out.println(error.toString()),
+                () -> System.out.println("complete"));
     }
 }
