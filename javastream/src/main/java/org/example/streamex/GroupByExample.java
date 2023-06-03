10c9f6f0174945fdc5744ccf543c7111a9b7acc1
hyuk <hyuk@woowahan.com>
2023-06-03 12:10:51 +0900

    study : stream groupBy

---
+++ javastream/src/main/java/org/example/streamex/GroupByExample.java
@@ -0,0 +1,9 @@
+package org.example.streamex;
+
+public class GroupByExample {
+    /*
+     * Stream 안의 데이터에 classifier를 적용했을 때 결과값이 같은 값끼리 List로 묶어 Map으로 반환
+     * 이 때 key는 classitier의 결과값이고 value는 List
+     *
+     * */
+}
---
+++ javastream/src/test/java/org/example/streamex/GroupByExampleTest.java
@@ -0,0 +1,44 @@
+package org.example.streamex;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class GroupByExampleTest {
+
+
+    @Test
+    void case1() {
+        List<Integer> numbers = List.of(123, 524, 342, 125, 64, 3, 234, 478, 345, 27, 8, 564);
+        Map<Integer, List<Integer>> unitDigitToNumbersMap = numbers.stream()
+            .collect(Collectors.groupingBy(number -> number % 10));
+        System.out.println(unitDigitToNumbersMap);
+    }
+
+    @Test
+    void case2() {
+        List<Integer> numbers = List.of(123, 524, 342, 125, 64, 3, 234, 478, 345, 27, 8, 564, 123, 523, 125, 64, 3, 234, 478, 111);
+        Map<Integer, Set<Integer>> unitDigitToNumbersMap = numbers.stream()
+            .collect(Collectors.groupingBy(number -> number % 10, Collectors.toSet()));
+        System.out.println(unitDigitToNumbersMap);
+    }
+
+    @Test
+    void case3() {
+        List<Integer> numbers = List.of(123, 524, 342, 125, 64, 3, 234, 478, 345, 27, 8, 564, 123, 523, 125, 64, 3, 234, 478, 111);
+        Map<Integer, Long> unitDigitToNumbersMap = numbers.stream()
+            .collect(Collectors.groupingBy(number -> number % 10, Collectors.counting()));
+        System.out.println(unitDigitToNumbersMap);
+    }
+
+    @Test
+    void case4() {
+        List<Integer> numbers = List.of(123, 524, 342, 125, 64, 3, 234, 478, 345, 27, 8, 564, 123, 523, 125, 64, 3, 234, 478, 111);
+        Map<Integer, List<String>> unitDigitToNumbersMap = numbers.stream()
+            .collect(Collectors.groupingBy(number -> number % 10, Collectors.mapping(number -> "value : " + number, Collectors.toList())));
+        System.out.println(unitDigitToNumbersMap);
+    }
+}
