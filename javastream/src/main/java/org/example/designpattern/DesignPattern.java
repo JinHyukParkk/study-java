b2c4a0bdd7f5f58ecba2c322800e5836c1684159
hyuk <hyuk@woowahan.com>
2023-06-18 23:58:03 +0900

    docs : 디자인 패턴 설명

---
+++ javastream/src/main/java/org/example/designpattern/DesignPattern.java
@@ -0,0 +1,11 @@
+package org.example.designpattern;
+
+public class DesignPattern {
+
+    /*
+     * 생성 패턴(Creational Pattern) - 객체의 생성과 초기화에 관련된 패턴
+     * 구조 패턴(Structural Pattern) - 클래스나 객체를 조합해 더 큰 구조를 만드는 패턴
+     * 행위 패턴(Behavioral Pattern) - 객체나 클래스 사이의 알고리즘이나 책임 분배에 관련된 패턴
+     * */
+
+}
