b1b35a7b68d61014b1c633c58f55e23209eec24c
hyuk <hyuk@woowahan.com>
2023-07-18 23:55:26 +0900

    Lazy 테스트

--- reactive/src/test/java/com/example/reactive/FluxExampleTest.java
+++ reactive/src/test/java/com/example/reactive/FluxExampleTest.java
@@ -1,9 +1,12 @@
 package com.example.reactive;
 
-import java.time.Duration;
-import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.IntStream;
 
 class FluxExampleTest {
 
@@ -18,9 +21,9 @@ class FluxExampleTest {
     @Test
     void test1() {
         Flux.fromIterable(List.of("foo", "bar"))
-                .doOnNext(System.out::println)
-                .map(String::toUpperCase)
-                .subscribe(System.out::println);
+            .doOnNext(System.out::println)
+            .map(String::toUpperCase)
+            .subscribe(System.out::println);
 
         System.out.println("비동기?");
     }
@@ -28,18 +31,38 @@ class FluxExampleTest {
     @Test
     void errorTest() {
         Flux.error(new IllegalStateException())
-                .doOnEach(System.out::println)
-                .subscribe();
+            .doOnEach(System.out::println)
+            .subscribe();
     }
 
     @Test
     void intervalTest() throws InterruptedException {
         Flux.interval(Duration.ofMillis(100))
-                .take(10)
-                .subscribe(System.out::println);
+            .take(10)
+            .subscribe(System.out::println);
 
         System.out.println("비동기?");
 
         Thread.sleep(5000);
+    }
+
+    @Test
+    @DisplayName("Flux 와 List를 비교해본다")
+    void fluxVsList() {
+        System.out.println("List");
+        IntStream.range(1, 11)
+            .peek(i -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            })
+            .forEach(System.out::println);
+
+        System.out.println("Flux");
+        Flux.range(1, 10)
+            .delayElements(Duration.ofSeconds(1))
+            .subscribe(System.out::println);
     }
 }
--- reactive/src/test/java/com/example/reactive/MainTest.java
+++ reactive/src/test/java/com/example/reactive/MainTest.java
@@ -2,4 +2,9 @@ package com.example.reactive;
 
 class MainTest {
 
+    /*
+    reatcive 에서 가장 중요했던 것
+    1. Thread의 Blocking을 최소화하는 코드를 작성해라
+    2. Lazily하게 값이 evalueate 되어야 한다.
+     */
 }
