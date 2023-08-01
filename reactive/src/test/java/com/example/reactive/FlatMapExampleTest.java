ef708d80cbcb14a252326ddecaa4ceb05a81967d
hyuk <hyuk@woowahan.com>
2023-08-01 23:58:36 +0900

    flatMap 테스트

--- reactive/src/test/java/com/example/reactive/FlatMapExampleTest.java
+++ reactive/src/test/java/com/example/reactive/FlatMapExampleTest.java
@@ -1,38 +1,40 @@
 package com.example.reactive;
 
-import static reactor.core.scheduler.Schedulers.parallel;
-
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static reactor.core.scheduler.Schedulers.parallel;
 
 class FlatMapExampleTest {
 
     @Test
     void flatMapTest() {
         Flux.just("a", "b", "c", "d", "e", "f", "g", "h", "i")
-                .window(3)
-                .flatMap(l -> l.map(this::toUpperCase))
-                .doOnNext(System.out::println)
-                .blockLast();
+            .window(3)
+            .flatMap(l -> l.map(this::toUpperCase))
+            .doOnNext(System.out::println)
+            .blockLast();
     }
 
     @Test
     void flatMapParallelTest() {
         Flux.just("a", "b", "c", "d", "e", "f", "g", "h", "i")
-                .window(3)
-                .flatMap(l -> l.map(this::toUpperCase).subscribeOn(parallel()))
-                .doOnNext(System.out::println)
-                .blockLast();
+            .window(3)
+            .flatMap(l -> l.map(this::toUpperCase).subscribeOn(parallel()))
+            .doOnNext(System.out::println)
+            .blockLast();
     }
 
     @Test
     void concatMapTest() {
         Flux.just("a", "b", "c", "d", "e", "f", "g", "h", "i")
-                .window(3)
-                .concatMap(l -> l.map(this::toUpperCase).subscribeOn(parallel()))
-                .doOnNext(System.out::println)
-                .blockLast();
+            .window(3)
+            .concatMap(l -> l.map(this::toUpperCase).subscribeOn(parallel()))
+            .doOnNext(System.out::println)
+            .blockLast();
 
         // concatMap은 순서를 보장하기 때문에 parallel이 의미가 없다.
     }
@@ -40,12 +42,27 @@ class FlatMapExampleTest {
     @Test
     void flatMapSequentialTest() {
         Flux.just("a", "b", "c", "d", "e", "f", "g", "h", "i")
-                .window(3)
-                .flatMapSequential(l -> l.map(this::toUpperCase).subscribeOn(parallel()))
-                .doOnNext(System.out::println)
-                .blockLast();
+            .window(3)
+            .flatMapSequential(l -> l.map(this::toUpperCase).subscribeOn(parallel()))
+            .doOnNext(System.out::println)
+            .blockLast();
 
         // 비동기적인데 병렬이 가능하고, 순서가 보장된다.
+    }
+
+    @Test
+    void convertFlux() {
+        Mono<List<String>> monoOfList = Mono.just(
+            List.of("사과", "바나나", "딸기", "포도")
+        );
+
+        Flux<String> flattenedFlux = monoOfList.flatMapMany(Flux::fromIterable);
+
+        flattenedFlux.subscribe(
+            value -> System.out.println("변환된 값: " + value),
+            error -> System.err.println("에러 발생: " + error),
+            () -> System.out.println("스트림 종료")
+        );
     }
 
     public List<String> toUpperCase(String s) {
