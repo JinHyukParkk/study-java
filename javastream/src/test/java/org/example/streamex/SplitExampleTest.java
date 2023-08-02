06677d1f9393dece09463370c5733232a4ec7548
hyuk <hyuk@woowahan.com>
2023-08-02 23:53:20 +0900

    test : Pattern splitAsStream 테스트

--- javastream/src/test/java/org/example/streamex/SplitExampleTest.java
+++ javastream/src/test/java/org/example/streamex/SplitExampleTest.java
@@ -14,4 +14,14 @@ class SplitExampleTest {
 
         stream.forEach(System.out::println);
     }
+
+    @Test
+    void case2() {
+        String text = "Hello,world!How,are,you?";
+
+        Stream<String> stream = Pattern.compile("[,!]")
+            .splitAsStream(text);
+
+        stream.forEach(System.out::println);
+    }
 }
