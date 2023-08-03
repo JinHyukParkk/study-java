3bec376ee87a7ad684ab0cc65d3ebca063f4d3ad
hyuk <hyuk@woowahan.com>
2023-08-03 23:14:34 +0900

    test : backpressure 테스트

--- reactive/src/test/java/com/example/reactive/BackpressureExampleTest.java
+++ reactive/src/test/java/com/example/reactive/BackpressureExampleTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
@@ -17,21 +18,21 @@ class BackpressureExampleTest {
      */
     @Test
     @DisplayName("그냥 4번 찍어봄")
-     void test() {
+    void test() {
         Flux<Long> flux = Flux.interval(Duration.ofMillis(100)).take(4).log();
         flux.doOnNext(System.out::println)
             .blockLast();
     }
 
     /*
-    * StepVerifier 는 verify 관련 메서드로 끝내야 함
-    * */
+     * StepVerifier 는 verify 관련 메서드로 끝내야 함
+     * */
     @Test
     @DisplayName("첫번째 요소를 가져와 순차적으로 확인하는 방법")
     void test2() {
         Flux<Integer> flux = Flux.just(0, 1, 2, 3).log();
 
-        StepVerifier.create(flux ,1)
+        StepVerifier.create(flux, 1)
             .expectNext(0)
             .thenRequest(1)    // 다음 Request 요청
             .expectNext(1)
@@ -94,5 +95,37 @@ class BackpressureExampleTest {
                     System.out.println("FINISH");
                 }
             });
+    }
+
+    @Test
+    @DisplayName("Backpressure 예제")
+    void test6() throws InterruptedException {
+        // Create a Flux that emits integers from 1 to 1000
+        Flux<Integer> numbersFlux = Flux.range(1, 1000);
+
+        // Simulate a slow subscriber by consuming elements with a delay
+        numbersFlux
+            .log()
+            .doOnNext(i -> {
+                // Simulate some slow processing
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            })
+            // Subscribe on a separate scheduler to introduce concurrency
+            .subscribeOn(Schedulers.parallel())
+            .subscribe(
+                // onNext: Print the received element
+                num -> System.out.println("Received: " + num),
+                // onError: Handle errors
+                Throwable::printStackTrace,
+                // onComplete: Called when the Flux completes
+                () -> System.out.println("Completed!")
+            );
+
+        // Wait for a while to observe backpressure in action
+        Thread.sleep(10000);
     }
 }
