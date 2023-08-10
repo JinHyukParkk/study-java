65cc2d292401af5e61fd768560db1daa9c1dcb38
hyuk <hyuk@woowahan.com>
2023-08-10 23:59:25 +0900

    test : distinct 테스트

--- reactive/src/test/java/com/example/reactive/MonoExampleTest.java
+++ reactive/src/test/java/com/example/reactive/MonoExampleTest.java
@@ -1,5 +1,6 @@
 package com.example.reactive;
 
+import io.reactivex.rxjava3.core.Flowable;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 
@@ -8,9 +9,9 @@ class MonoExampleTest {
     @Test
     void test() {
         Mono.just(100L)
-                .map(i -> i * 2)
-                .or(Mono.just(1L))
-                .subscribe(System.out::println);
+            .map(i -> i * 2)
+            .or(Mono.just(1L))
+            .subscribe(System.out::println);
 
         System.out.println("비동기?!");
     }
@@ -23,6 +24,18 @@ class MonoExampleTest {
     @Test
     void errorTest() {
         Mono.error(new IllegalAccessException())
-                .subscribe(System.out::println);
+            .subscribe(System.out::println);
     }
+
+    @Test
+    void distinctTest() {
+
+        Flowable<Integer> numbers = Flowable.just(1, 2, 2, 3, 4, 4, 5);
+
+        Flowable<Integer> distinctNumbers = numbers.distinct();
+
+        distinctNumbers.subscribe(System.out::println);
+
+    }
+
 }
