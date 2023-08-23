ff4439c4caf5cd5293894cb19a9d5624cba5fdff
hyuk <hyuk@woowahan.com>
2023-08-23 23:31:38 +0900

    test : flatmap 테스트

--- javastream/src/test/java/org/example/streamex/FlatMapExampleTest.java
+++ javastream/src/test/java/org/example/streamex/FlatMapExampleTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class FlatMapExampleTest {
 
     @Test
+    @DisplayName("Array -> Stream flatMap 간단 예시")
     void case1() {
         String[][] cities = new String[][]{
             {"Seoul", "Busan"},
@@ -21,15 +24,32 @@ class FlatMapExampleTest {
         };
 
         List<String> cityList = Arrays.stream(cities)
-            .flatMap(city -> List.of(city).stream())
+            .flatMap(Stream::of)
             .collect(Collectors.toList());
 
         System.out.println(cityList);
     }
 
+
+    @Test
+    @DisplayName("List -> Stream flatMap 간단 예시")
+    void case2() {
+        List<List<Integer>> listOfLists = Arrays.asList(
+            Arrays.asList(1, 2, 3),
+            Arrays.asList(4, 5, 6),
+            Arrays.asList(7, 8, 9)
+        );
+
+        List<Integer> flattenedList = listOfLists.stream()
+            .flatMap(Collection::stream)
+            .collect(Collectors.toList());
+
+        System.out.println(flattenedList); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+
     @Test
     @DisplayName("flatMap을 이용한 Order -> OrderLine 변환")
-    void case2() {
+    void case3() {
         List<Order> orders = OrderFixtureFactory.createOrderWithOrderLine();
 
         List<OrderLine> orderLineList = orders.stream()
@@ -41,7 +61,7 @@ class FlatMapExampleTest {
 
     @Test
     @DisplayName("flatMap을 이용한 Order -> OrderLine 변환 2")
-    void case3() {
+    void case4() {
         List<Order> orders = OrderFixtureFactory.createOrderWithOrderLine();
 
         List<OrderLine> orderLineList = orders.stream()
