ace1b5595b34ca7d3f637745b26ba435c5773d5f
hyuk <hyuk@woowahan.com>
2023-08-24 23:54:21 +0900

    test : partitioningBy 테스트

--- javastream/src/test/java/org/example/streamex/PartitioningByExampleTest.java
+++ javastream/src/test/java/org/example/streamex/PartitioningByExampleTest.java
@@ -10,12 +10,31 @@ import java.util.stream.Collectors;
 class PartitioningByExampleTest {
 
     @Test
-    @DisplayName("조건에 따라 분류한다")
+    @DisplayName("홀수 짝수, 조건에 따라 분류한다")
     void case1() {
         List<Integer> numbers = List.of(123, 524, 342, 125, 64, 3, 234, 478, 345, 27, 8, 564);
         Map<Boolean, List<Integer>> unitDigitToNumbersMap = numbers.stream()
-            .collect(Collectors.partitioningBy(number -> number % 2 == 0));
+                .collect(Collectors.partitioningBy(number -> number % 2 == 0));
 
         System.out.println(unitDigitToNumbersMap);
+    }
+
+    @Test
+    @DisplayName("소수 조건에 따라 분류한다")
+    void case2() {
+        List<Integer> numbers = List.of(2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        Map<Boolean, List<Integer>> partitioned = numbers.stream()
+                .collect(Collectors.partitioningBy(this::isPrime));
+
+        List<Integer> primeNumbers = partitioned.get(true);
+        List<Integer> notPrimeNumbers = partitioned.get(false);
+
+        System.out.println("소수: " + primeNumbers);
+        System.out.println("소수 아닌 수: " + notPrimeNumbers);
+    }
+
+    private boolean isPrime(int number) {
+        return number > 1 && java.util.stream.IntStream.range(2, number).noneMatch(index -> number % index == 0);
     }
 }
