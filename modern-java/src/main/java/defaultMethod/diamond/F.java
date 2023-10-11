bb0963def407b01b0c935389f5ced3562296980b
hyuk <hyuk@woowahan.com>
2023-10-11 23:59:25 +0900

    :memo: 인터페이스 테스트

--- modern-java/src/main/java/defaultMethod/diamond/F.java
+++ modern-java/src/main/java/defaultMethod/diamond/F.java
@@ -6,4 +6,8 @@ public class F implements A, A2 {
     public void hello() {
         A.super.hello();
     }
+
+    public void hello2() {
+        A2.super.hello();
+    }
 }
---
+++ modern-java/src/test/java/defaultMethod/diamond/FTest.java
@@ -0,0 +1,13 @@
+package defaultMethod.diamond;
+
+import org.junit.jupiter.api.Test;
+
+public class FTest {
+
+    @Test
+    void hello() {
+        new F().hello();
+        new F().hello2();
+    }
+
+}
