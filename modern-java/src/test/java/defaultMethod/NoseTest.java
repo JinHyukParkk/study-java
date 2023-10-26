69ea7603379b549bdbb47f398af47555bae9af3d
hyuk <hyuk@woowahan.com>
2023-10-26 10:41:49 +0900

    test

--- modern-java/src/test/java/defaultMethod/NoseTest.java
+++ modern-java/src/test/java/defaultMethod/NoseTest.java
@@ -4,11 +4,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 class NoseTest {
 
     @Test
     void defalutMethod() {
         assertThat(new Nose().isEmpty()).isTrue();
         assertThat(new Nose(10).isEmpty()).isFalse();
+    }
+
+    @Test
+    void test() {
+        List<String> list = Arrays.asList("a", "b", "c");
+
+        list.set(2, "d");
+//        list.add("d");
+
+        list.forEach(System.out::println);
     }
 }
