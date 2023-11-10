61dbabe7de0e5863dd219f4176628d96423249d0
hyuk <hyuk@woowahan.com>
2023-11-10 23:46:37 +0900

    :memo: 참조 투명한 펙토리얼 작성

--- modern-java/src/test/java/functionalInterface/function/FunctionExampleTest.java
+++ modern-java/src/test/java/functionalInterface/function/FunctionExampleTest.java
@@ -1,6 +1,8 @@
 package functionalInterface.function;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
+
 import org.junit.jupiter.api.Test;
 
 class FunctionExampleTest {
@@ -11,5 +13,23 @@ class FunctionExampleTest {
 
         int result = add.apply(3, 5);
         System.out.println(result);
+    }
+
+    @Test
+    void factorial() {
+        Function<Integer, Integer> factorial = n -> calculateFactorial(n);
+
+        int result1 = factorial.apply(5); // 결과는 120
+        int result2 = factorial.apply(5); // 결과도 120
+
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+    static int calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
     }
 }
